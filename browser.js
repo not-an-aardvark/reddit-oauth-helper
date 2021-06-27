8add8ffa0b5d45fc26116f0b5266f778c391c211
Harshit <harsjoshi1614@gmail.com>
2021-06-28 03:59:02 +0530

    added water.css for better styling & a Theme Switch ! (#21)

--- browser.js
+++ browser.js
@@ -37,7 +37,8 @@ class RedditOauthHelper extends React.Component {
       React.createElement(
         'h1',
         null,
-        'Reddit OAuth Helper'
+        'Reddit OAuth Helper ',
+        React.createElement(ThemeSwitcher, null)
       ),
       React.createElement(
         'h4',
@@ -171,12 +172,32 @@ class RedditOauthHelper extends React.Component {
   }
 }
 
+function ThemeSwitcher() {
+  const toggleTheme = () => {
+    const rootElm = document.querySelector(":root");
+    const theme = rootElm.getAttribute("theme");
+
+    if (theme === "dark") {
+      rootElm.setAttribute("theme", "light");
+    } else {
+      rootElm.setAttribute("theme", "dark");
+    }
+  };
+
+  return React.createElement(
+    'label',
+    { className: 'switch', onChange: toggleTheme },
+    React.createElement('input', { type: 'checkbox' }),
+    React.createElement('span', { className: 'slider round' })
+  );
+}
+
 function AnonymousTokenDisplay(props) {
   return React.createElement(
     'div',
     null,
     React.createElement(
-      'span',
+      'p',
       null,
       'Anonymous token (expires after 1 hour, cannot access account-specific information):',
       ' '
@@ -199,7 +220,7 @@ function ClientInfoInput(props) {
     'div',
     null,
     React.createElement(
-      'div',
+      'p',
       null,
       'Make sure to set the app redirect URI to ',
       React.createElement(
